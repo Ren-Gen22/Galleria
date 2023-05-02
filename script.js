ede81238b944938d767c40acb1ac68815a5282b4
Ren-Gen22 <abhishekrenjan33@gmail.com>
2023-05-02 22:24:02 +0530

    Custom popups for authentication

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.onscroll = () => {
   searchIcon.classList.remove("fa-times");
   searchForm.classList.remove("active");
 };
-
+/*
 //form validation
 const contactForm = document.querySelector(".contact form");
 
@@ -57,5 +57,55 @@ contactForm.addEventListener("submit", function (event) {
   }
 
   alert("Form submitted successfully!");
+  contactForm.submit();
+});*/
+
+const contactForm = document.querySelector(".contact form");
+
+function showPopupMessage(message) {
+  // Create the popup element
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+  popup.innerHTML = `<p>${message}</p>`;
+
+  // Append the popup to the contact section
+  const contactSection = document.querySelector(".contact");
+  contactSection.appendChild(popup);
+
+  // Hide the popup after 3 seconds
+  setTimeout(function () {
+    contactSection.removeChild(popup);
+  }, 3000);
+}
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.querySelector('.contact input[type="text"]').value;
+  const email = document.querySelector('.contact input[type="email"]').value;
+  const number = document.querySelector('.contact input[type="number"]').value;
+  const subject = document.querySelector(
+    '.contact input[type="text"]:nth-of-type(2)'
+  ).value;
+  const message = document.querySelector(".contact textarea").value;
+
+  if (!name || !email || !number || !subject || !message) {
+    showPopupMessage("Please fill in all the fields!");
+    return;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    showPopupMessage("Please enter a valid email address!");
+    return;
+  }
+
+  const phoneRegex = /^\d{10}$/;
+  if (!phoneRegex.test(number)) {
+    showPopupMessage("Please enter a 10-digit phone number!");
+    return;
+  }
+
+  showPopupMessage("Form submitted successfully!");
   contactForm.submit();
 });
