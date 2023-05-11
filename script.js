3842317ca0833aa6ff294b4d171c5f3cc1d53a77
Ren-Gen22 <abhishekrenjan33@gmail.com>
2023-05-11 23:01:38 +0530

    Noticification box updated

--- script.js
+++ script.js
@@ -24,10 +24,62 @@ window.onscroll = () => {
   searchIcon.classList.remove("fa-times");
   searchForm.classList.remove("active");
 };
-/*
-//form validation
-const contactForm = document.querySelector(".contact form");
 
+var ALERT_TITLE = "Alert";
+var ALERT_BUTTON_TEXT = "Ok";
+
+if (document.getElementById) {
+  window.alert = function (txt) {
+    createCustomAlert(txt);
+  };
+}
+
+function createCustomAlert(txt) {
+  d = document;
+
+  if (d.getElementById("modalContainer")) return;
+
+  mObj = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
+  mObj.id = "modalContainer";
+  mObj.style.height = d.documentElement.scrollHeight + "px";
+
+  alertObj = mObj.appendChild(d.createElement("div"));
+  alertObj.id = "alertBox";
+  if (d.all && !window.opera)
+    alertObj.style.top = document.documentElement.scrollTop + "px";
+  alertObj.style.left =
+    (d.documentElement.scrollWidth - alertObj.offsetWidth) / 2 + "px";
+  alertObj.style.visiblity = "visible";
+
+  h1 = alertObj.appendChild(d.createElement("h1"));
+  h1.appendChild(d.createTextNode(ALERT_TITLE));
+
+  msg = alertObj.appendChild(d.createElement("p"));
+  msg.innerHTML = txt;
+
+  btn = alertObj.appendChild(d.createElement("a"));
+  btn.id = "closeBtn";
+  btn.appendChild(d.createTextNode(ALERT_BUTTON_TEXT));
+  btn.href = "#";
+  btn.focus();
+  btn.onclick = function () {
+    removeCustomAlert();
+    return false;
+  };
+
+  alertObj.style.display = "block";
+}
+
+function removeCustomAlert() {
+  document
+    .getElementsByTagName("body")[0]
+    .removeChild(document.getElementById("modalContainer"));
+}
+function ful() {
+  alert("Alert this pages");
+}
+
+const contactForm = document.querySelector(".contact form");
 contactForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -55,57 +107,8 @@ contactForm.addEventListener("submit", function (event) {
     alert("Please enter a 10-digit phone number!");
     return;
   }
-
+  ALERT_TITLE = "Success";
   alert("Form submitted successfully!");
-  contactForm.submit();
-});*/
-
-const contactForm = document.querySelector(".contact form");
-
-function showPopupMessage(message) {
-  // Create the popup element
-  const popup = document.createElement("div");
-  popup.classList.add("popup");
-  popup.innerHTML = `<p>${message}</p>`;
-
-  // Append the popup to the contact section
-  const contactSection = document.querySelector(".contact");
-  contactSection.appendChild(popup);
-
-  // Hide the popup after 3 seconds
-  setTimeout(function () {
-    contactSection.removeChild(popup);
-  }, 3000);
-}
-
-contactForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const name = document.querySelector('.contact input[type="text"]').value;
-  const email = document.querySelector('.contact input[type="email"]').value;
-  const number = document.querySelector('.contact input[type="number"]').value;
-  const subject = document.querySelector(
-    '.contact input[type="text"]:nth-of-type(2)'
-  ).value;
-  const message = document.querySelector(".contact textarea").value;
-
-  if (!name || !email || !number || !subject || !message) {
-    showPopupMessage("Please fill in all the fields!");
-    return;
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    showPopupMessage("Please enter a valid email address!");
-    return;
-  }
-
-  const phoneRegex = /^\d{10}$/;
-  if (!phoneRegex.test(number)) {
-    showPopupMessage("Please enter a 10-digit phone number!");
-    return;
-  }
-
-  showPopupMessage("Form submitted successfully!");
+  ALERT_TITLE = "Alert";
   contactForm.submit();
 });
